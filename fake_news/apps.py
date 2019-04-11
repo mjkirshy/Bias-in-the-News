bcbf63f4430087d935a0ad837a3667522267ebe5
Ryan Smith <rmsmith12@gmail.com>
2019-04-11 09:44:52 -0400

    List of VS Code deletions

--- fake_news/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class FakeNewsConfig(AppConfig):
-    name = 'fake_news'
--- fake_news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Publisher
-
-class FormName(forms.ModelForm):
-    class Meta:
-        model = Publisher
-        fields = ['article_address',]
