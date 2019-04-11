09981fd49fb622ff3791109cb2c5115bb44992e1
Ryan Smith <rmsmith12@gmail.com>
2019-04-11 19:36:22 -0400

    Attempted database is locked fix, only able to test via azure deployment.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'connect_timeout': 5, ## added to help prevent another database is locked error 
+        }
     }
 }
 
