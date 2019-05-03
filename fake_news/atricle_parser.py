eda3f8b1d507e007902576baabee71be69594211
Matthew Kirshy <mjkirshy@buffalo.edu>
2019-05-03 11:50:20 -0400

    added cosine similarity matrix to file in attempt to find summary of article using cosine similarity and text preprocessing

--- fake_news/atricle_parser.py
+++ fake_news/atricle_parser.py
@@ -1,5 +1,13 @@
 from newspaper import Article
 from smmryapi.smmryapi import SmmryAPI
+import nltk
+import numpy as np
+import networkx as nx
+from nltk.cluster.util import cosine_distance
+from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+from nltk.corpus import stopwords
+from googleapiclient.discovery import build
+import json
 
 def article_parser_fn(link):
     ##for all information regarding 'article' objects, please see newspaper3k documentation
@@ -14,3 +22,60 @@ def article_smmry_fn(link):
     url = link
     s = smmry.summarize(url, sm_length=3, sm_keyword_count=5)
     return(s)
+
+service = build("customsearch", "v1", developerKey="AIzaSyCR5BPcANHoawEeKzbaKFZdoJQqSR-GABY")
+ 
+def read_article(file_text):
+    #file = open(file_path, "r")
+    ##INSERT FILE NAME IN FUNCTION CALL BELOW######
+    #bcr = PlaintextCorpusReader(file_path, 'bernie.txt')
+    filedata = file_text
+    #filedata = bcr.raw()
+    #for word in filedata.split():
+    #    if word == 'Mr.':
+    #        filedata[word] = 'Mr'
+    article = filedata.replace("\n\n", '. ').replace('Mr.', 'Mr').replace("\r", ' ').replace('\n', ' ').split('. ')
+    articlez = []
+    for line in article:
+        if line == '':
+            continue
+        if line[0] == '\n':
+            line = line[1:]
+        articlez.append(line)
+    sentences = []
+    for sentence in articlez:
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop() 
+    
+    return sentences
+
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+    all_words = list(set(sent1 + sent2))
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+    return 1 - cosine_distance(vector1, vector2)
+ 
+def build_similarity_matrix(sentences, stop_words):
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: 
+                continue 
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+    return similarity_matrix
+
+
+#####INSERT FILE PATH IN THE FUNCTION CALL BELOW!!!###########
+#generate_summary( "C:\\Users\\matthew.kirshy\\Desktop\\CSE-442-Fake-News-Web-Application\\article_db\\wapo\\by_article", 2)
